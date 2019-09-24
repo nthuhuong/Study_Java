d624d8088c675e9fe45148eea8252da273ffba28
nthuhuong <ntthuong@fossil.com>
2019-09-25 06:08:13 +0700

    Write more test methods for the sort array int function

--- src/arrays/SortIntNumber.java
+++ src/arrays/SortIntNumber.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SortIntNumber {
 
-    private static void swap(int[] arr, int firstIndex, int secondIndex) {
+    private static void swap(final int[] arr, final int firstIndex, final int secondIndex) {
 
         if (firstIndex >= arr.length ||
             secondIndex >= arr.length ||
@@ -22,7 +22,7 @@ public class SortIntNumber {
 
 
 
-    private static int[] sortArrayNumber(final int[] arrayNumber) {
+    public static int[] sortArrayNumber(final int[] arrayNumber) {
         if (arrayNumber == null) {
             return null;
         }
@@ -30,7 +30,6 @@ public class SortIntNumber {
         if (arrayNumber.length == 0) {
             return new int[] {};
         }
-
 
         for (int i = 0; i < arrayNumber.length - 1; i++) {
             for (int j = i + 1 ; j < arrayNumber.length; j++) {
@@ -44,56 +43,62 @@ public class SortIntNumber {
     }
 
 
-//    public static void main(String[] agrs) {
-//        int[] arrInt = {10};//, 4, 20, 7, 3, 9, 24, 90};
-//        System.out.println(arrInt.length);
-//        sortArrayNumber(arrInt);
-//        for (int i : arrInt) {
-//            System.out.println(i);
-//        }
-//    }
-
 
     @Test
     public void testNullArray() {
-        int[] arrInt = null;
-        assertArrayEquals(null, sortArrayNumber(arrInt), "Error when enter parameter is null");
+        assertArrayEquals(null, sortArrayNumber(null), "Error when enter parameter is null");
     }
 
     @Test
     public void testEmptyArray() {
-        // Arrange
-        int[] arrInt = {};
-        int[] expected = {};
-
-        // Act
-        int[] actual = sortArrayNumber(arrInt);
-
-        // Assert
-        assertArrayEquals(expected, actual, "Error when enter parameter is Empty");
+        assertArrayEquals(new int[] {}, sortArrayNumber(new int[] {}), "Error when enter parameter is Empty");
     }
 
     @Test
     public void testResultArray() {
+        assertArrayEquals(new int[] {1, 3, 7, 18, 59, 94}, sortArrayNumber(new int[] {3, 59, 7, 94, 1, 18 }),
+                "Result is fail" );
+    }
+
+    @Test
+    public void testInputArrayBeChanged() {
         int[] arrInt = {3, 59, 7, 94, 1, 18 };
-        int[] expected = {1, 3, 7, 18, 59, 94};
-
-        int[] actual = sortArrayNumber(arrInt);
-
-        assertArrayEquals(expected, actual, "Result is fail" );
+        assertArrayEquals(arrInt, sortArrayNumber(arrInt), "Result is fail" );
     }
 
     @Test
     public void testOneElementArray() {
-        int[] arrInt = {10};
-        int[] expected = {10};
+        assertArrayEquals(new int[] {10}, sortArrayNumber(new int[] {10}),
+                "Error when enter array have one element");
+    }
 
+    @Test
+    public void testArrayHaveElementsSame() {
+        assertArrayEquals(new int[] {3, 5, 5, 5, 9, 11, 20, 45}, sortArrayNumber(new int[] {5, 3, 9, 5, 5, 20, 11, 45}),
+                "Error when enter array have elements same together");
+    }
+
+    @Test
+    public void testArrayBeSorted() {
+        assertArrayEquals(new int[] {3, 4, 5, 7, 8, 10, 11, 23}, sortArrayNumber(new int[] {3, 4, 5, 7, 8, 10, 11, 23}),
+                "Error when enter array be sorted");
+    }
+
+    @Test
+    public void testArrayHaveAllElementSame() {
+        assertArrayEquals(new int[] {4, 4, 4, 4, 4, 4}, sortArrayNumber(new int[] {4, 4, 4, 4, 4, 4}),
+                "Error when enter array have all elements same");
+    }
+
+    @Test
+    public void testPreviuosElementSmallerThanNextElement() {
+        int[] arrInt = {5, 10, 4, 8, 30, 25, 6, 15};
         int[] actual = sortArrayNumber(arrInt);
 
-        assertArrayEquals(expected, actual, "Error when enter array have one element");
+        for (int i = 0; i < actual.length -1; i++) {
+            assertTrue(actual[i] <= actual[i + 1],"Error Array is not properly sorted");
+        }
     }
-    
-
 
 }
 
