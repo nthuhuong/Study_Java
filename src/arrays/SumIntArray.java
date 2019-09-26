d876d63062b582947d0d4cd1ce2f138aa5d7e66f
nthuhuong <ntthuong@fossil.com>
2019-09-26 14:59:38 +0700

    Write Unit test for function to sum values of an array

---
+++ src/arrays/SumIntArray.java
@@ -0,0 +1,49 @@
+package arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class SumIntArray {
+
+    public static int sumArray(int[] arrInt) {
+
+        int sum = 0;
+
+        for (int temp : arrInt) {
+            sum += temp;
+        }
+
+        return sum;
+    }
+
+    @Test
+    public void testResultSum() {
+        assertEquals(74, sumArray(new int[] {1, 5, 23, 45}), "Result returned is fail");
+    }
+
+    @Test
+    public void testEmptyArray() {
+        assertEquals(0, sumArray(new int[] {}),"Error when enter array empty");
+    }
+
+    @Test
+    public void testArrayHasAllElementsZero() {
+        assertEquals(0, sumArray(new int[] {0, 0, 0, 0, 0}),"Error when enter a array has all elements zero");
+    }
+
+    @Test
+    public void tetsArrayHasElementsNegativeNumber() {
+        assertEquals(-7, sumArray(new int[] {-10, 1, 2}),"Error when enter array has contain elements negative number");
+    }
+
+    @Test
+    public void testArrayHasAllElementsNegativeNumber() {
+        assertEquals(-22, sumArray(new int[] {-16, -3, -3}),"Error when enter array has all elements negative number");
+    }
+
+    @Test
+    public void testArrayHasNegativeZeroPositive() {
+        assertEquals(0, sumArray(new int[] {-3, 0, 3}),"Error when array contain negative number, zero and positive number");
+    }
+
+}
